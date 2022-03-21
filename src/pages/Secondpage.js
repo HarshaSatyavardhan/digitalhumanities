0af8e976bef75a480d522e0e908397d109326875
specialized boy <41695878+HarshaSatyavardhan@users.noreply.github.com>
2022-03-21 15:09:17 +0530

    added word cloud

--- src/pages/Secondpage.js
+++ src/pages/Secondpage.js
@@ -2,12 +2,20 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+// import JupyterViewer from "react-jupyter-notebook";
+// import nb_test from "./digital_humanitie.json";
+import { IpynbRenderer } from "react-ipynb-renderer";
+import "katex/dist/katex.min.css";
+
+// Jupyter theme
+import "react-ipynb-renderer/dist/styles/monokai.css";
+
+import ipynb from "./digital_humanitie.json";
 
 const Secondpage = () => {
   return (
     <div>
       <Navbar />
-
       <Typography
         sx={{ textAlign: "center", p: 2 }}
         variant="h4"
@@ -16,6 +24,24 @@ const Secondpage = () => {
       >
         Word Cloud
       </Typography>
+      <IpynbRenderer
+        ipynb={ipynb}
+        syntaxTheme="xonokai"
+        language="python"
+        bgTransparent={true}
+        formulaOptions={{
+          // optional
+          renderer: "mathjax", // katex by default
+          katex: {
+            delimiters: "gitlab", // dollars by default
+            katexOptions: {
+              fleqn: false,
+            },
+          },
+        }}
+      />
+
+      {/* <JupyterViewer rawIpynb={nb_test} /> */}
     </div>
   );
 };
